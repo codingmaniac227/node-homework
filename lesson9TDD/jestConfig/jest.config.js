45fd9df2e4e7e198807e14c2877a3a1821df684d
John R. McGarvey <johnmcgarvey55@gmail.com>
2025-10-10 19:42:24 -0400

    fix tdd for lessons 8 and 9 to handle passport removal

--- lesson9TDD/jestConfig/jest.config.js
+++ lesson9TDD/jestConfig/jest.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   rootDir: "../../.",
   moduleNameMapper: {
-    ".*validation/taskSchema.*": "<rootDir>/lesson11TDD/mocks/taskSchema.js",
-    ".*validation/userSchema.*": "<rootDir>/lesson11TDD/mocks/userSchema.js",
-    ".*controllers/taskController.*": "<rootDir>/lesson11TDD/mocks/taskController.js",
-    ".*controllers/userController.*": "<rootDir>/lesson11TDD/mocks/userController.js",
-    ".*services/userService.*":  "<rootDir>/lesson11TDD/mocks/userService.js",
-    ".*passport/passport.*":  "<rootDir>/lesson11TDD/mocks/passport.js",
+    ".*validation/taskSchema.*": "<rootDir>/lesson9TDD/mocks/taskSchema.js",
+    ".*validation/userSchema.*": "<rootDir>/lesson9TDD/mocks/userSchema.js",
+    ".*controllers/taskController.*": "<rootDir>/lesson9TDD/mocks/taskController.js",
+    ".*controllers/userController.*": "<rootDir>/lesson9TDD/mocks/userController.js",
+    ".*services/userService.*":  "<rootDir>/lesson9TDD/mocks/userService.js",
   },
   reporters: [
     "default",
-    "./lesson11TDD/jestConfig/TDDReporter.js"
+    "./lesson9TDD/jestConfig/TDDReporter.js"
   ]
 };
